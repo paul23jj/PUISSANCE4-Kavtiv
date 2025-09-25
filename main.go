89bf5137904eaaa1d3e30e849c511a4fb07e8f0d
cauchi <enzocauchi12gmail.com>
2025-09-25 10:07:58 +0200

    j ai corriger les beug de paul vu que fabio fou rien

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"./router"
+)
+
+func main() {
+	// Charge le routeur
+	r := router.New()
+
+	fmt.Println("🚀 Serveur démarré sur http://localhost:8080")
+	http.ListenAndServe(":8080", r)
+}
---
+++ router/routeur.go
@@ -0,0 +1,20 @@
+package router
+
+import (
+	"net/http"
+
+	"../controller"
+)
+
+// New crée et retourne un nouvel objet ServeMux configuré avec les routes de l'application
+func New() *http.ServeMux {
+
+	mux := http.NewServeMux() // Création d'un nouveau ServeMux, qui est un routeur simple pour les requêtes HTTP
+
+	// On associe les chemins URL à des fonctions spécifiques du controller
+	mux.HandleFunc("/", controller.Home)           // "/" correspond à la page d'accueil. Appelle la fonction Home du controller
+	mux.HandleFunc("/about", controller.About)     // "/about" correspond à la page "À propos". Appelle la fonction About
+	mux.HandleFunc("/contact", controller.Contact) // "/contact" correspond à la page de contact. Appelle la fonction Contact
+
+	return mux // On retourne le routeur configuré
+}
